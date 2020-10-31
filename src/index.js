2552e6a255a4e430645d35386f6320a576bee028
Matt Garber <mgarber93@gmail.com>
2020-10-30 20:26:38 -0400

    Add linting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 // middlewares
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
@@ -14,18 +13,18 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
 // configure routes
-app.get('/', (req, res) => {
-    res.send('This is the landing page\nSam was here\nRon was also here')
+app.get("/", (req, res) => {
+  res.send("This is the landing page\nSam was here\nRon was also here");
 });
 
-app.get('/home', (req, res) => {
-    res.send('This is the home page');
-})
+app.get("/home", (req, res) => {
+  res.send("This is the home page");
+});
 
-app.get('/health', (req, res) => {
-    res.send('Healthy!');
-})
+app.get("/health", (req, res) => {
+  res.send("Healthy!");
+});
 
 app.listen(port, () => {
-    console.log(`Trail Finder is listening on port http://localhost:${port}`);
+  console.log(`Trail Finder is listening on port http://localhost:${port}`);
 });
