36378b543ab1f0b8edd8eaa6b1bdf565b9440061
Matt Garber <mgarber93@gmail.com>
2020-11-05 03:16:26 -0500

    Remove unneeded copy
    This technically isn't needed aslong as you start from /dist/index (like npm run start does).

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,6 @@ module.exports = {
           from: path.resolve(__dirname, 'src/views'),
           to: path.resolve(__dirname, 'dist/views'),
         },
-        {
-          from: path.resolve(__dirname, '.env'),
-          to: path.resolve(__dirname, 'dist'),
-        },
       ],
       options: {
         concurrency: 100,
