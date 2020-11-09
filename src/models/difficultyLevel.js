d7a39ef2a1ce9c8915d9449c0ade0786595d83ce
sammyson101 <samueldchristensen@gmail.com>
2020-11-08 16:15:47 -0800

    update for demo

---
+++ src/models/difficultyLevel.js
@@ -0,0 +1,8 @@
+export const DifficultyLevel = {
+  GREEN: 0,
+  GREENBLUE: 1,
+  BLUE: 2,
+  BLUEBLACK: 3,
+  BLACK: 4,
+  BLACKBLACK: 5,
+};
---
+++ src/models/user.js
@@ -0,0 +1,10 @@
+export class User {
+  constructor(id, userName, password, email, displayName, difficulyLevel) {
+    this.id = id;
+    this.userName = userName;
+    this.password = password;
+    this.email = email;
+    this.displayName = displayName;
+    this.difficulyLevel = difficulyLevel;
+  }
+}
