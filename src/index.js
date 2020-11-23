d6ecf8862bb65131acaf673d54fd1482c5be5d9a
Matt Garber <mgarber93@gmail.com>
2020-11-22 20:00:29 -0500

    Simplify imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { trailsRouter, newUserRouter, authRouter, nearbyRoute } from './routes';
 import { isLoggedOn, addMiddlewares } from './middlewares';
-import { ziptoLatLon } from './controllers';
-import { findTrailsNear, findDistanceToTrail } from './controllers';
+import {
+  findTrailsNear,
+  findDistanceToTrail,
+  ziptoLatLon,
+} from './controllers';
 
 const express = require('express');
 
