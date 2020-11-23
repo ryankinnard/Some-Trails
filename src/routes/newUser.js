420ed56ed56bc83e651797b06562d712981caf4c
sammyson101 <samueldchristensen@gmail.com>
2020-11-22 16:17:07 -0800

    User Update
    Fix spelling error in User class
    Pass results of answered question to be parsed to create a user difficulty
    Pass 0 as user ID that will then be changed at on the server side to be records.length

--- src/routes/newUser.js
+++ src/routes/newUser.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { User } from '../models';
+import { DifficultyLevel, User } from '../models';
 import * as users from '../controllers/users';
 
 const router = express.Router();
@@ -9,17 +9,45 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', function (req, res) {
+  console.log(req.body);
+
   const newUser = new User(
-    2,
+    0,
     req.body.username,
     req.body.password,
     req.body.email,
     req.body.displayName,
-    3,
+    parseDifficulty(
+      (parseInt(req.body.q1) +
+        parseInt(req.body.q2) +
+        parseInt(req.body.q3) +
+        parseInt(req.body.q4) +
+        parseInt(req.body.q5)) /
+        6,
+    ),
   );
 
+  console.log(newUser);
+
   users.pushUser(newUser);
-  res.render('home', { user: newUser });
+  res.render('home', { user: 0 });
 });
+
+function parseDifficulty(dl) {
+  switch (dl) {
+    case dl < 1:
+      return DifficultyLevel.GREEN;
+    case dl < 2:
+      return DifficultyLevel.GREENBLUE;
+    case dl < 3:
+      return DifficultyLevel.BLUE;
+    case dl < 4:
+      return DifficultyLevel.BLUEBLACK;
+    case dl < 5:
+      return DifficultyLevel.BLACK;
+    default:
+      return DifficultyLevel.BLACKBLACK;
+  }
+}
 
 export const newUserRouter = router;
