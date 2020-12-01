d9fda7711105c8d7ee0d8adfe645805403a5a5a6
mgarber93 <mgarber93@gmail.com>
2020-11-30 23:37:59 -0500

    Add auth router (#15)
    * Add auth router
    
    * Remove comment
    
    * Add nearby to index
    
    * Fix export
    
    * Simplify imports
    
    * Fix merge errors
    
    Co-authored-by: sammyson101 <samueldchristensen@gmail.com>

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -16,8 +16,22 @@ router.post(
 );
 
 router.get('/logout', function (req, res) {
-  req.logout();
-  res.redirect('/');
+  let gear = {
+    temp: 30,
+    elevationGain: 2005,
+    boots: 'https://www.flaticon.com/svg/static/icons/svg/2826/2826618.svg',
+    lightJacket:
+      'https://www.flaticon.com/svg/static/icons/svg/3126/3126039.svg',
+    heavyJacket: 'https://www.flaticon.com/svg/static/icons/svg/614/614247.svg',
+    poles: 'https://www.flaticon.com/svg/static/icons/svg/2325/2325148.svg',
+    desBoots:
+      'Wear a solid pair of hiking boots as the terrain can be challenging',
+    desHeavyJacket: "Bring a heavier jacket, it's below 32°F",
+    desLightJacket: "Bring a lighter jacket, it's above 32°F",
+    desPoles: 'Bring hiking poles, the elevation gain is more than 2000 feet',
+  };
+  req.session.gear = gear;
+  res.redirect('nearby');
 });
 
 export const authRouter = router;
