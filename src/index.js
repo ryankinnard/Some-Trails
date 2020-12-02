30f33a2930fcec28c9e19e2bac9c041e20ac43af
Adam Bolton <44854081+agbolton@users.noreply.github.com>
2020-12-02 16:39:11 -0500

    Revert "Min max (#34)"
    This reverts commit 0b64baf503f641d0553c55fb8f655852052db6c5.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import {
-  findTrailsNear,
-  findDistanceToTrail,
-  ziptoLatLon,
-} from './controllers';
-import {
-  getDifficultyIconPath,
-  parseDifficultyFromNum,
-  parseDifficultyFromObject,
-} from './models';
-import {
   trailsRouter,
   newUserRouter,
   authRouter,
@@ -19,7 +9,6 @@ import {
 import { isLoggedOn, addMiddlewares } from './middlewares';
 
 import { getDifficultyIconPath, parseDifficultyFromNum } from './models';
-
 
 const express = require('express');
 
@@ -70,41 +59,6 @@ app.get('/profile', isLoggedOn, function (req, res) {
 });
 
 app.post('/newuser', newUserRouter);
-
-
-app.post('/search', async function redirectToSearch(req, res) {
-  let gear = {
-    icon: 'https://www.flaticon.com/svg/static/icons/svg/545/545674.svg',
-    water: 'https://www.flaticon.com/svg/static/icons/svg/606/606797.svg',
-    food: 'https://www.flaticon.com/svg/static/icons/svg/1046/1046857.svg',
-    boots: 'https://www.flaticon.com/svg/static/icons/svg/2826/2826618.svg',
-    poles: 'https://www.flaticon.com/svg/static/icons/svg/2325/2325148.svg',
-    desInfo:
-      'Hover over the icons to the right to see gear, water and food recommendations for this trail',
-    desWaterThree: 'Bring 3 liters of water, trail is longer than eight miles',
-    desWaterOne: 'Bring at least a liter of water',
-    desFoodOne: 'Bring at least one snack',
-    desFoodTwo: 'Bring at least two snacks, trail is longer than five miles',
-    desBoots:
-      'Wear a solid pair of hiking boots as the terrain can be challenging',
-    desPoles: 'Bring hiking poles, the elevation gain is more than 700 feet',
-  };
-  const coordinate = await ziptoLatLon(req.body.zip);
-  const trails = await findTrailsNear(coordinate);
-  let count = 0;
-  trails.forEach((element, index, array) => {
-    element.distance = findDistanceToTrail(element, coordinate);
-    element.time = element.length / 2 + 0.5 * (element.ascent / 1000);
-    element.difficulty = parseDifficultyFromObject(element);
-  });
-
-  const results = trails.filter(
-    (element) =>
-      element.difficulty <= req.body.max && element.difficulty >= req.body.min,
-  );
-
-  res.render('search-results', { results: results, gear: gear });
-});
 
 // start server
 app.listen(port, () => {
